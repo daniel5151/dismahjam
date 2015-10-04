b11c846bc82d37d8f0b05a786f2c9d044f9a4f54
Daniel Prilik <danielprilik@gmail.com>
2015-10-04 11:24:11 -0400

    jeammies!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 window.onload = function () {
-    
+    $('#stats-button').click(function(){
+        if ($('.jam-stats').height() > 0) {
+            $('.jam-stats').height(0);
+        } else {
+            $('.jam-stats').height("290");
+        }
+    })
 }
