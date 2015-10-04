9be3316661574174a2afe107a20a9e7c5f3de77a
Daniel Prilik <danielprilik@gmail.com>
2015-10-04 13:06:38 -0400

    we jammin'

--- js/main.js
+++ js/main.js
@@ -3,9 +3,33 @@ window.onload = function () {
         if ($('.jam-stats').height() > 0) {
             $('.jam-stats').height(0);
         } else {
-            $('.jam-stats').height("290");
+            $('.jam-stats').height("350");
         }
+    });
+
+    changeJam();
+
+    $("#nah").click(function(){
+    	changeJam()
     })
+}
+
+function changeJam() {
+	var jamArr = {
+    	brand:"smuckers",
+    	flavor:"strawberry", 
+    	cost:"$170000000 ZWL",
+    	jamminess: "jammy",
+    	popular: 0
+    };
+
+    $("#j_brand").text(jamArr.brand);
+    $("#j_flavor").text(jamArr.flavor);
+    $("#j_cost").text(jamArr.cost);
+    $("#j_jamminess").text(jamArr.jamminess);
+    $("#j_popular").text(jamArr.popular);
+
+    $(".img-container img").attr("src","jams/"+jamArr.brand+"/"+jamArr.flavor+".jpg")
 }
 
 function switchDiv(num){
